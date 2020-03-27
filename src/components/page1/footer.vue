c0e8a461f282bade4e75a714389d26922edb3e1b
Drama Queen <dramaqueen@macintoshs-MacBook-Air.local>
2020-03-27 05:19:00 +0100

    this is the second trial on the footer region, why are there errors? why can't i call the rest of the value? Is my code correct

--- src/components/page1/footer.vue
+++ src/components/page1/footer.vue
@@ -1,8 +1,12 @@
 <template>
  <div class="container mt-5">
   <div class="row" >
-    <div class="col-sm return" v-for="(list, i) in item" :key="i">
-      {{list.toUpperCase()}}
+    <div class="col-sm return" v-for="(list, i) in item" :key="i" >
+      <div v-for="(val, key) in list" :key="i">{{key.toUpperCase()}}</div>
+      <div class="d-flex flex-column bd-highlight mb-3">
+  <div class="p-2 bd-highlight" v-for="(val, key) in list" :key="i">{{val[0]}}</div>
+  
+</div>
     </div>
     
   </div>
@@ -13,7 +17,12 @@
 export default {
   data() {
     return {
-        item:['company', 'service', 'orders & returns', 'payment accept']
+        item:[
+          {Company : ['Home', 'About Us', 'Shop', 'Blog', 'Contact Us']},
+          {Service:['Support', 'Faq', 'Warranty', 'Live Chat', 'Privacy Policy']},
+          {'Orders & Returns': ['Order', 'Status', 'Shipping', 'Policy & Services','Cart']},
+          {'Payment Accept': ''}
+        ]
     };
   }
 };
