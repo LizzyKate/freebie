6c28298dde2f3d7afbde0a93c3d28b02de918b47
bemijonathan <bemijonathan@gmail.com>
2020-03-30 08:00:53 +0100

    changed file name

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 <script>
 import  './assets/css/main.css';
 import './assets/css/style.css';
-import home from './components/page1/Home.vue'
+import home from './components/page1/home.vue'
 
 export default {
   components:{
