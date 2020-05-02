c2c641e655ac17a184c234595a38d0a88e1883f7
Drama Queen <dramaqueen@macintoshs-MacBook-Air.local>
2020-05-02 17:13:52 +0100

    please work

--- src/route.js
+++ src/route.js
@@ -3,7 +3,7 @@ import shop from './components/page 2/shop';
 import cart from './components/page 3/cart'
 import blog from './components/page 4/blog';
 import info from './components/page 5/info';
-import about from './components/page 6/about';
+import about from './components/page 6/About';
 import contact from './components/page 7/contact';
 import questions from './components/additions/questions';
 import checkout from './components/additions/checkout/sum';
