50c13fed95b24661f21dd8f14a28a0cffe5443e4
bemijonathan <bemijonathan@gmail.com>
2020-05-02 16:41:18 +0100

    remove all bootstrap vue

--- src/route.js
+++ src/route.js
@@ -1,24 +1,24 @@
 import home from './components/page1/home';
-import shop from './components/page 2/shop';
-import cart from './components/page 3/cart'
-import blog from './components/page 4/blog';
-import info from './components/page 5/info';
-import about from './components/page 6/about';
-import contact from './components/page 7/contact';
+import shop from './components/page2/shop';
+import cart from './components/page3/cart'
+import blog from './components/page4/blog';
+import info from './components/page5/info';
+import about from './components/page6/About';
+import contact from './components/page7/contact';
 import questions from './components/additions/questions';
 import checkout from './components/additions/checkout/sum';
 import drop from './components/additions/drop'
 
-export default[
-    {path:'/',component: home},
-    {path:'/shop', name:'shop', component:shop},
-    {path:'/cart/', component:cart},
-    {path:'/cart/:id', component:cart},
-    {path:'/blog', component:blog},
-    {path:'/info', component:info},
-    {path:'/about', component:about},
-    {path:'/contact', component:contact},
-    {path:'/questions', component:questions},
-    {path:'/checkout', component:checkout},
-    {path:'/drop', component:drop}
+export default [
+    { path: '/', component: home },
+    { path: '/shop', name: 'shop', component: shop },
+    { path: '/cart/', component: cart },
+    { path: '/cart/:id', component: cart },
+    { path: '/blog', component: blog },
+    { path: '/info', component: info },
+    { path: '/about', component: about },
+    { path: '/contact', component: contact },
+    { path: '/questions', component: questions },
+    { path: '/checkout', component: checkout },
+    { path: '/drop', component: drop }
 ]
